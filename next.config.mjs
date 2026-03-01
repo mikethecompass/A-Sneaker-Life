96e534b9f0ee286d5b3ea760904f09aa9313ec92
MikeTheCompass <mikethecompass@gmail.com>
2026-03-01 01:49:42 -0700

    add dks.scene7.com image domain

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const nextConfig = {
       { protocol: "https", hostname: "img.youtube.com" },
       { protocol: "https", hostname: "images.stockx.com" },
       { protocol: "https", hostname: "**.scene7.com" },
+      { protocol: "https", hostname: "dks.scene7.com" },
       { protocol: "https", hostname: "**.shopify.com" },
       { protocol: "https", hostname: "**.nike.com" },
       { protocol: "https", hostname: "**.adidas.com" },
